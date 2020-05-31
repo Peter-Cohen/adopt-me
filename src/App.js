200e80eacec83a29dc4181bd9c4a7a87405b6577
Peter-Cohen <peter.cohen.1969@gmail.com>
2020-05-31 15:26:33 +0200

    Finished section "Tools"

--- src/App.js
+++ src/App.js
@@ -1,31 +1,22 @@
-const Pet = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Luna"),
-    React.createElement("h2", {}, "Dog"),
-    React.createElement("h2", {}, "Havanese"),
-  ])
-}
-
-
-
-
-
+import React from 'react';
+import { render } from 'react-dom';
+import { Pet } from './Pet';
 
 const App = () => {
-  return React.createElement(
-    'div',
-    { id: "something-important"},
-    [
-      React.createElement('h1', {}, 'Adopt Me!'),
-      React.createElement(Pet),
-      React.createElement(Pet),
-      React.createElement(Pet),
-  ]
-    
-  );
+  return React.createElement("div", { id: "something-important" }, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cockatiel",
+    }),
+    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mixed" }),
+  ]);
 };
 
-ReactDOM.render(
-  React.createElement(App),
-  document.getElementById('root')
-); 
+render(React.createElement(App), document.getElementById("root"));
